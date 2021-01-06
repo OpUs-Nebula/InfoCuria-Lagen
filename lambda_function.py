326395fcb82e8582b5a732778be5fd59f2ad1dd8
Mbwenga Maliti <mbwenga.maliti@gmail.com>
2021-01-06 16:40:55 +0100

    Inconsistent use of tabs and spaces.

--- lambda_function.py
+++ lambda_function.py
@@ -4,15 +4,15 @@ import InfoCuria.search_scraping as IC
 import Lagen.search_scraping as LG
 
 def case_by_source_type(title):
-	if True in list(map(lambda court: court in title,["JO"])):
-		return LG.case_by_id(title)
-	else:
-		return IC.case_by_id(title)
+    if True in list(map(lambda court: court in title,["JO"])):
+        return LG.case_by_id(title)
+    else:
+        return IC.case_by_id(title)
 
 def lambda_handler(event, context):
     # TODO implement
     if event["isBase64Encoded"]:
-    	base64_bytes = event["body"].encode("ascii") 
+        base64_bytes = event["body"].encode("ascii") 
         string_bytes = base64.b64decode(base64_bytes) 
         event["body"] = string_bytes.decode("ascii") 
     
@@ -23,8 +23,8 @@ def lambda_handler(event, context):
     }
 
 if __name__ == "__main__":
-	#print(IC.case_by_id("C-131/12 (Mario Costeja González – the right to be forgotten)"))
-	print("Script Done")
-	print(LG.case_by_id("JO 2016-01-05 dnr 7041-2013 (Radering av videoinspelningar)"))
-	event = {"body":"JO, 2014-09-09, dnr 3032-2011"}
-	lambda_handler(event,None)
+    #print(IC.case_by_id("C-131/12 (Mario Costeja González – the right to be forgotten)"))
+    print("Script Done")
+    print(LG.case_by_id("JO 2016-01-05 dnr 7041-2013 (Radering av videoinspelningar)"))
+    event = {"body":"JO, 2014-09-09, dnr 3032-2011"}
+    lambda_handler(event,None)
