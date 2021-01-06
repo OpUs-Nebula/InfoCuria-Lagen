8ad4d52d4d4cb88a2a606ac9569b0bd24f33b1fb
Mbwenga Maliti <mbwenga.maliti@gmail.com>
2021-01-06 16:36:05 +0100

    Request parsing

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import base64
 import InfoCuria.search_scraping as IC
 import Lagen.search_scraping as LG
 
@@ -10,13 +11,20 @@ def case_by_source_type(title):
 
 def lambda_handler(event, context):
     # TODO implement
+    if event["isBase64Encoded"]:
+    	base64_bytes = event["body"].encode("ascii") 
+        string_bytes = base64.b64decode(base64_bytes) 
+        event["body"] = string_bytes.decode("ascii") 
+    
     case_title = event["body"].strip()
     return {
         'statusCode': 200,
-        'body': case_by_source_type(case_title)
+        'body': json.dumps(case_by_source_type(case_title))
     }
 
 if __name__ == "__main__":
 	#print(IC.case_by_id("C-131/12 (Mario Costeja González – the right to be forgotten)"))
 	print("Script Done")
 	print(LG.case_by_id("JO 2016-01-05 dnr 7041-2013 (Radering av videoinspelningar)"))
+	event = {"body":"JO, 2014-09-09, dnr 3032-2011"}
+	lambda_handler(event,None)
