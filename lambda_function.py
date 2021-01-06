9230449cab73749171accd3e129399a60aa13a1d
Mbwenga Maliti <mbwenga.maliti@gmail.com>
2021-01-06 17:45:36 +0100

    Encoding/Decoding fix

--- lambda_function.py
+++ lambda_function.py
@@ -12,9 +12,8 @@ def case_by_source_type(title):
 def lambda_handler(event, context):
     # TODO implement
     if event["isBase64Encoded"]:
-        base64_bytes = event["body"].encode("ascii") 
-        string_bytes = base64.b64decode(base64_bytes) 
-        event["body"] = string_bytes.decode("ascii") 
+        string_bytes = base64.b64decode(event["body"]) 
+        event["body"] = string_bytes.decode() 
     
     case_title = event["body"].strip()
     return {
@@ -26,5 +25,7 @@ if __name__ == "__main__":
     #print(IC.case_by_id("C-131/12 (Mario Costeja González – the right to be forgotten)"))
     print("Script Done")
     print(LG.case_by_id("JO 2016-01-05 dnr 7041-2013 (Radering av videoinspelningar)"))
-    event = {"body":"JO, 2014-09-09, dnr 3032-2011"}
-    lambda_handler(event,None)
+    events = [{"body":"Qy0xMzEvMTIgKE1hcmlvIENvc3RlamEgR29uesOhbGV6IOKAkyB0aGUgcmlnaHQgdG8gYmUgZm9yZ290dGVuKQ==",
+    "isBase64Encoded":True},{"body":"Sk8gMjAxOS0wNi0wNCwgZG5yIDY3OTQtMjAxNw==","isBase64Encoded":True}]
+    results = list(map(lambda event: lambda_handler(event,None),events))
+    print(results)
